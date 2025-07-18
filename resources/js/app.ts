5028af7d0abda1bb4375d14980884e160d96eac6
dneb8 <107234871+dneb8@users.noreply.github.com>
2025-07-17 23:52:53 -0600

    feat: utilizar applayout globalmente

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,18 +1,25 @@
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 import { initializeTheme } from './composables/useAppearance';
 import { Icon } from '@iconify/vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
+    resolve: async (name) => {
+        const pages = import.meta.glob<DefineComponent>('./pages/**/*.vue');
+        const page = (await pages[`./pages/${name}.vue`]()) as any;
+        page.default.layout ??= AppLayout;
+
+        return page;
+    },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
