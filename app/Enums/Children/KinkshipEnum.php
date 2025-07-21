f1ce59be9aacda466ff18a120d83c40cf55dac25
ligcd <liligg161@gmail.com>
2025-07-21 12:43:57 -0600

    1-Enum Kinkship
    - Creación de la carpeta enum de Children
    - Creación del archivo kinkshipenum donde se ingresan datos como Hijo, Sobrino, Primo, Hermano etc.

---
+++ app/Enums/Children/KinkshipEnum.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Enums\Children;
+
+enum KinkshipEnum: string
+{
+    case HIJO = 'hijo';
+    case SOBRINO = 'sorino';
+    case PRIMO = 'primo';
+    case HERMANO = 'hermano';
+    case OTRO = 'otro';
+
+    public function label(): string
+    {
+        return match ($this) {
+            static::HIJO => 'Hij@',
+            static::SOBRINO => 'Sobrin@',
+            static::PRIMO => 'Prim@',
+            static::HERMANO => 'Herman@',
+            static::OTRO=>'Otro'
+        };
+    }
+}
+
