b65f950a294b16eb5906c251e42b5ea168f94aeb
dneb8 <107234871+dneb8@users.noreply.github.com>
2025-08-02 18:41:24 -0600

    feat: mostrar rol de usuario en cards9

---
+++ resources/js/pages/User/components/UserCard.vue
@@ -0,0 +1,28 @@
+<!-- components/UserCard.vue -->
+<script setup lang="ts">
+import type { User } from '@/types/User'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+
+defineProps<{
+  user: User
+  columns: string[] // Ej. ['name', 'email', 'roles']
+}>()
+</script>
+
+<template>
+  <Card class="max-h-48">
+    <CardHeader>
+      <CardTitle>{{ user.name }} {{ user.first_surname }} {{ user.last_surname }}</CardTitle>
+      <CardDescription class="hyphens-manual">{{ user.email }}</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <div><strong>Número:</strong> {{ user.number }}</div>
+      <div v-for="role in user.roles" :key="role.id"><strong>Roles:</strong> {{ role.name}}</div>
+      <!-- Add more fields as needed -->
+    </CardContent>
+    <CardFooter>
+      <!-- footer si es necesario -->
+    </CardFooter>
+  </Card>
+</template>
+
