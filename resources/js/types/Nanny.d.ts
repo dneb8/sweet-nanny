7d00acde84136d723d68e371c0811496309ce679
dneb8 <107234871+dneb8@users.noreply.github.com>
2025-08-25 20:38:12 -0600

    feat: completar tipos de interfaces

--- resources/js/types/Nanny.d.ts
+++ resources/js/types/Nanny.d.ts
@@ -1,3 +1,4 @@
+import User from '.User';
 export interface Nanny {
   ulid: number;
   user_id: number;
@@ -6,4 +7,12 @@ export interface Nanny {
   start_date: string; 
   address_id?: number;
   address?: Address; 
+
+  user?:User;
+  address?: Address;
+  courses?: Course[];
+  careers?: Career[];
+  qualities?: Quality[];
+  reviews?: Review[];
+  booking_services?: BookingService[];
 }
