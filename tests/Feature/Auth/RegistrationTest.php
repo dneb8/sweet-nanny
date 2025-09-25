67dccf13530fc6c6467beb174e2ba0228b37f52d
dneb8 <107234871+dneb8@users.noreply.github.com>
2025-09-25 11:31:37 -0600

    fix: test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -11,7 +11,7 @@ test('new users can register', function () {
         'name' => 'Test User',
         'email' => 'test@example.com',
         'password' => 'password123!',
-        'password_confirmation' => 'password',
+        'password_confirmation' => 'password123!',
     ]);
 
     $this->assertAuthenticated();
