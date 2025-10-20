6005be34edc17c41cb6aee14bb8cd4eee56c6aee
dneb8 <deneb.rivera0272@alumnos.udg.mx>
2025-10-19 18:03:20 -0600

    Update DegreeEnum.php

--- app/Enums/Career/DegreeEnum.php
+++ app/Enums/Career/DegreeEnum.php
@@ -4,28 +4,35 @@ namespace App\Enums\Career;
 
 enum DegreeEnum: string
 {
-    case BACHILLERATO = 'bachillerato';
-    case TECNICO = 'tecnico';
-    case LICENCIATURA = 'licenciatura';
-    case MAESTRIA = 'maestria';
-    case DOCTORADO = 'doctorado';
+    case LICENCIATURA = 'Licenciatura';
+    case MAESTRIA = 'Maestría';
+    case DOCTORADO = 'Doctorado';
+    case TECNICO = 'Técnico';
+    case DIPLOMADO = 'Diplomado';
+    case CERTIFICACION = 'Certificación';
 
     public function label(): string
     {
         return match ($this) {
-            static::BACHILLERATO => 'Bachillerato',
-            static::TECNICO      => 'Carrera Técnica',
-            static::LICENCIATURA => 'Licenciatura',
-            static::MAESTRIA     => 'Maestría',
-            static::DOCTORADO    => 'Doctorado',
+            self::LICENCIATURA => 'Licenciatura',
+            self::MAESTRIA => 'Maestría',
+            self::DOCTORADO => 'Doctorado',
+            self::TECNICO => 'Técnico',
+            self::DIPLOMADO => 'Diplomado',
+            self::CERTIFICACION => 'Certificación',
         };
     }
 
-    public static function toArray(): array
+    public static function values(): array
     {
-        $values = array_column(self::cases(), 'value');
-        $labels = array_map(fn ($case) => $case->label(), self::cases());
-        
-        return array_combine($values, $labels);
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function labels(): array
+    {
+        return array_combine(
+            self::values(),
+            array_map(fn($case) => $case->label(), self::cases())
+        );
     }
 }
