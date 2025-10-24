741d04db8737d5bf396629681695a2943737aa17
ligcd <liligg161@gmail.com>
2025-10-23 23:09:36 -0600

    Moficación del factory Course
    Se modifica el factory de course, se eliminan nombres y solo se dejan organizaciones que se general al azar al igual que los nomrbes

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\{Course, Nanny};
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Enums\Course\NameEnum;
 
 class CourseFactory extends Factory
 {
@@ -11,26 +12,23 @@ class CourseFactory extends Factory
 
     // Lista de cursos reales y organizaciones mexicanas
     protected $courses = [
-        ['name' => 'Primeros Auxilios', 'organization' => 'Cruz Roja Mexicana'],
-        ['name' => 'Educación Infantil', 'organization' => 'Universidad Pedagógica Nacional'],
-        ['name' => 'Cuidado de Niños con Necesidades Especiales', 'organization' => 'Instituto de Psicología Infantil'],
-        ['name' => 'Nutrición Infantil', 'organization' => 'Universidad Autónoma de México'],
-        ['name' => 'Seguridad y Prevención de Accidentes', 'organization' => 'Protección Civil'],
-        ['name' => 'Desarrollo Cognitivo en Niños', 'organization' => 'Centro de Estudios Infantiles'],
-        ['name' => 'Psicología Infantil Básica', 'organization' => 'Universidad Nacional Autónoma de México'],
-        ['name' => 'Lenguaje y Comunicación', 'organization' => 'Centro de Educación Infantil'],
-        ['name' => 'Taller de Juegos Educativos', 'organization' => 'Escuela de Pedagogía'],
-        ['name' => 'Primeros Auxilios Psicológicos', 'organization' => 'Cruz Roja Mexicana'],
-        ['name' => 'Danza Folclórica Mexicana', 'organization' => 'CECATI 157'],
-        ['name' => 'Danza Contemporánea', 'organization' => 'Centro Cultural Ollin Yoliztli'],
-        ['name' => 'Ballet Clásico', 'organization' => 'Escuela Nacional de Danza Clásica y Contemporánea'],
-        ['name' => 'Música Infantil', 'organization' => 'Centro Cultural Tlalpan'],
-        ['name' => 'Artes Plásticas para Niños', 'organization' => 'Museo Tamayo'],
-        ['name' => 'Taller de Teatro Infantil', 'organization' => 'Instituto Nacional de Bellas Artes'],
-        ['name' => 'Educación Socioemocional', 'organization' => 'SEP'],
-        ['name' => 'Lengua Extranjera: Inglés Básico', 'organization' => 'CONALEP'],
-        ['name' => 'Programación y Robótica Infantil', 'organization' => 'Tecnológico de Monterrey'],
-        ['name' => 'Manualidades y Creatividad', 'organization' => 'Casa de Cultura'],
+        ['organization' => 'Cruz Roja Mexicana'],
+        ['organization' => 'Universidad Pedagógica Nacional'],
+        ['organization' => 'Instituto de Psicología Infantil'],
+        ['organization' => 'Universidad Autónoma de México'],
+        ['organization' => 'Protección Civil'],
+        ['organization' => 'Centro de Estudios Infantiles'],
+        ['organization' => 'Universidad Nacional Autónoma de México'],
+        ['organization' => 'Centro de Educación Infantil'],
+        ['organization' => 'Escuela de Pedagogía'],
+        ['organization' => 'Cruz Roja Mexicana'],
+        ['organization' => 'CECATI 190'],
+        ['organization' => 'CECATI 16'],
+        ['organization' => 'CECATI 15'],
+        ['organization' => 'SEP'],
+        ['organization' => 'CONALEP'],
+        ['organization' => 'Tecnológico de Monterrey'],
+        ['organization' => 'Casa de Cultura'],
     ];
 
     public function definition(): array
@@ -39,7 +37,7 @@ class CourseFactory extends Factory
         $course = $this->faker->randomElement($this->courses);
 
         return [
-            'name' => $course['name'],
+            'name' => $this->faker->randomElement(NameEnum::values()),
             'organization' => $course['organization'],
             'date' => $this->faker->dateTimeBetween('-5 years', 'now')->format('Y-m-d'),
             'nanny_id' => Nanny::inRandomOrder()->first()?->id,
